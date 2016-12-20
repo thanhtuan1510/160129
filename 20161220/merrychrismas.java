c58aaeacb2cca454a397c2709c5ac36cb29f078c
thanhtuan1510 <zodiac.libra.1510@gmail.com>
2016-12-20 09:57:49 +0900

    merrychrismas.java added.

---
+++ 20161220/merrychrismas.java
@@ -0,0 +1,10 @@
+public class merrychrismas  {
+	public static void main(String[] args) {
+		for(int i = 0; i<20; i++){
+			System.out.println("MERRY CHRISMAS AND HAPPY NEW YEAR!");
+			
+		}
+		
+	}
+	
+}
