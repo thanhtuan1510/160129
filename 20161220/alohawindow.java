c314544d2cf4d66027a9ecec5f760a95c204d154
thanhtuan1510 <zodiac.libra.1510@gmail.com>
2016-12-20 10:43:58 +0900

    alohawindow.java added.

---
+++ 20161220/alohawindow.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class alohawindow  {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(200,200);
+		frame.setVisible(true);
+
+	}
+	
+}
